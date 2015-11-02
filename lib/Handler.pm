76695ed34696c3b9dea95842dab6cf8abe316cc4
Pedro <pedro.uriarte@globant.com>
2015-11-02 11:46:23 -0300

    create class Handler.

---
+++ lib/Handler.pm
@@ -0,0 +1,24 @@
+#
+#===============================================================================
+#
+#         FILE: Handler.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 02/11/15 11:45:08
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+package Handler;
+
+1;
+
