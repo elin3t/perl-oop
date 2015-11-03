42dabb6a462c2de15bce2d436ce220969d6e24d9
Eliecer Daza <eliecer.daza@globant.com>
2015-11-03 12:10:18 -0300

    ReceptionCmd Package empty test file

---
+++ lib/ReceptionCmd.pm
@@ -0,0 +1,16 @@
+package ReceptionCmd;
+use strict;
+use warnings FATAL => 'all';
+use parent 'Command';
+
+use OrderService;
+
+sub execute{
+    my $self = shift;
+    my @parameters = $self->parameters();
+    my $order_s = OrderService->new();
+    return $order_s->reception_package($parameters[0], $parameters[1], $parameters[2]);
+}
+
+
+1;
---
+++ t/receptioncmd.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Test::More;
+
+use lib '../lib';
+use ReceptionCmd;
+
+
+
+done_testing();
+
