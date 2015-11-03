a0bfc5cdd1afc284cd97b1bfb4ee1f10b561e3d5
Eliecer Daza <eliecer.daza@globant.com>
2015-11-03 13:49:39 -0300

    ReceptionCmd Package and test

--- lib/ReceptionCmd.pm
+++ lib/ReceptionCmd.pm
@@ -9,8 +9,7 @@ sub execute{
     my $self = shift;
     my @parameters = $self->parameters();
     my $order_s = OrderService->new();
-    return $order_s->reception_package($parameters[0], $parameters[1], $parameters[2]);
+    return $order_s->reception_package($parameters[0], $parameters[1], $parameters[2], $parameters[3]);
 }
-
 
 1;
--- t/receptioncmd.t
+++ t/receptioncmd.t
@@ -6,7 +6,10 @@ use Test::More;
 use lib '../lib';
 use ReceptionCmd;
 
+my @parameters = ('12345', '54321', 'Cabillito JM Moreno 200', '20151102');
+my $recpetioncmd = ReceptionCmd->new(@parameters);
+
+is( $recpetioncmd->execute(), 1, 'Get and return the value of sending the args to reception_package');
 
 
 done_testing();
-
