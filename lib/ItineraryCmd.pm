f490b9ade1d367772bc31019ed5fb918033c36fc
Pedro <pedro.uriarte@globant.com>
2015-11-03 10:32:18 -0300

    task-41 class ItineraryCmd.

---
+++ lib/ItineraryCmd.pm
@@ -0,0 +1,37 @@
+#
+#===============================================================================
+#
+#         FILE: ItineraryCmd.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 02/11/15 17:14:20
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use Command;
+use OrderService;
+
+package ItineraryCmd;
+
+sub execute {
+    my $self = shift;
+    my @parameters = $self->parameters;
+    my $order_number = $parameters[0];
+    my $order_service = OrderService->new();
+    my $output = $order_service->read_itinerary($order_number);
+
+    return $output;
+}
+
+1;
+
+
