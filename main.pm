b59575f188965f1d72d05420fc1a51ab90924478
Eliecer Daza <eliecer.daza@globant.com>
2015-11-04 10:05:10 -0300

    add default input file and open with UTF-8 encoding

--- main.pm
+++ main.pm
@@ -8,10 +8,10 @@ use Data::Dumper;
 use Handler;
 
 sub main{
-    my $filetoread = shift @ARGV || die "no file to read!";
+    my $filetoread = shift @ARGV || "input.txt" || die "no file to read!";
     my $filetowrite = shift @ARGV || "output.txt";
     my $filetowriteerrors = shift @ARGV || "error.txt";
-    open my $entries, $filetoread or "Could not open the file: $filetoread";
+    open (my $entries, '<:encoding(UTF-8)' ,$filetoread) or "Could not open the file: $filetoread";
     open(my $output, '>', $filetowrite) or die "Could not open file '$filetowrite' $!";
     open(my $errors, '>', $filetowriteerrors) or die "Could not open file '$filetowriteerrors' $!";
     my $handle = Handler->new();
